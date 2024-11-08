4ae32f52343697c219f5e6437e58dcfd626a39b2
Souma <101255979+5ouma@users.noreply.github.com>
2024-11-08 19:36:58 +0900

    chore(component): Add Astrobook for homepage
    Preview whether it's rendered correctly.

---
+++ src/components/Homepage/story.ts
@@ -0,0 +1,5 @@
+import type { Props } from "./Homepage.astro";
+
+export const Domain: Props = { url: "https://example.com" };
+export const Path: Props = { url: "https://www.example.com/path" };
+export const Japanese: Props = { url: "https://www.example.com/パス" };
