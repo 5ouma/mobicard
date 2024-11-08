2aecf4d7ae2bb872e22ecf60e553126d5f6d546d
Souma <101255979+5ouma@users.noreply.github.com>
2024-11-08 21:18:41 +0900

    test(vitest): Exclude Astro and Astrobook related (#8)
    They don't need to be tested.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,20 @@
 /// <reference types="vitest" />
 
 import { getViteConfig } from "astro/config";
+import { coverageConfigDefaults } from "vitest/config";
 
 export default getViteConfig({
   test: {
     reporters: ["verbose", ["junit", { suiteName: "Component Tests" }]],
     outputFile: "junit.xml",
+    coverage: {
+      exclude: [
+        "astro.config.ts",
+        "src/pages/",
+        "src/layouts/",
+        "**/*.stories.ts",
+        ...coverageConfigDefaults.exclude,
+      ],
+    },
   },
 });
