50f544bebe530ea306cea1bf5f6a167ea92c11e3
Souma <101255979+5ouma@users.noreply.github.com>
2024-11-16 14:36:34 +0900

    chore(component): Add Astrobook for sns
    Preview all variants with icons and ids.

---
+++ src/components/SNS/story.ts
@@ -0,0 +1,41 @@
+import type { Props } from "./SNS.astro";
+
+export const Bluesky: Props = {
+  service: "Bluesky",
+  id: "bsky.app",
+};
+
+export const Facebook: Props = {
+  service: "Facebook",
+  id: "facebook",
+};
+
+export const GitHub: Props = {
+  service: "GitHub",
+  id: "github",
+};
+
+export const Instagram: Props = {
+  service: "Instagram",
+  id: "instagram",
+};
+
+export const Mastodon: Props = {
+  service: "Mastodon",
+  id: "@Mastodon@mastodon.social",
+};
+
+export const Misskey: Props = {
+  service: "Misskey",
+  id: "@support@misskey.io",
+};
+
+export const Posts: Props = {
+  service: "Posts",
+  id: "support",
+};
+
+export const Twitter: Props = {
+  service: "Twitter",
+  id: "twitter",
+};
