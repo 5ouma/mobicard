f510d567ed18b3b4a97df232df183394270af8d3
Souma <101255979+5ouma@users.noreply.github.com>
2024-11-16 14:56:43 +0900

    feat(page): Change to the new components
    Add new information to the page.

--- src/components/Homepage/story.ts
+++
@@ -1,5 +0,0 @@
-import type { Props } from "./Homepage.astro";
-
-export const Domain: Props = { url: "https://example.com" };
-export const Path: Props = { url: "https://www.example.com/path" };
-export const Japanese: Props = { url: "https://www.example.com/パス" };
