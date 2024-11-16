e4e1cf9211a207dae5eda570ad83c6a1dd149ea7
Souma <101255979+5ouma@users.noreply.github.com>
2024-11-16 14:03:22 +0900

    chore(component): Add Astrobook for access
    Preview all variants with domains and email address.

---
+++ src/components/Access/Access.stories.ts
@@ -0,0 +1,11 @@
+import Access from "./Access.astro";
+import * as stories from "./story.ts";
+
+export default {
+  title: "Homepage and Email",
+  component: Access,
+};
+
+export const Default = { args: stories.Default };
+export const SubDomain = { args: stories.SubDomain };
+export const WithoutEmail = { args: stories.WithoutEmail };
