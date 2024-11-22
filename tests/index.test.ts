83c3a4df8e287491ff2807f7af61e256539d28c3
Souma <101255979+5ouma@users.noreply.github.com>
2024-11-22 20:08:47 +0900

    style(test): Move the test directory to the root
    It's not a source file actually.

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,7 +1,7 @@
 import { experimental_AstroContainer as AstroContainer } from "astro/container";
 import { expect, test } from "vitest";
 
-import Index from "../pages/index.astro";
+import Index from "../src/pages/index.astro";
 
 test("Index", async () => {
   const container: AstroContainer = await AstroContainer.create();
