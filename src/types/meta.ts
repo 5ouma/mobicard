3d66a84aaae43019f10e2514469d6a2fb9c80602
Souma <101255979+5ouma@users.noreply.github.com>
2024-12-27 13:19:30 +0900

    docs(js): Add JSDoc for better development (#158)
    It's easy to understand what it is.

--- src/types/meta.ts
+++ src/types/meta.ts
@@ -1,5 +1,33 @@
 import type { serviceName } from "./services.ts";
 
+/**
+ * Meta data of the site.
+ *
+ * @property name Site name
+ * @property description Site description
+ * @property icon Site icon URL
+ * @property homepage Homepage URL
+ * @property email Email address
+ * @property sns SNS information
+ * @property card Site URL
+ *
+ * @example
+ * ```ts
+ * import { meta } from "./types/meta.ts";
+ * const site: meta = {
+ *   name: "Site Name",
+ *   description: "Site description",
+ *   icon: "https://example.com/icon.png",
+ *   homepage: "https://example.com",
+ *   email: "email@example.com",
+ *   sns: [
+ *     { service: "Twitter", id: "twitter" },
+ *     { service: "GitHub", id: "github" },
+ *   ],
+ *   card: "https://example.com",
+ * };
+ * ```
+ */
 export type meta = Readonly<{
   name: string;
   description: string;
