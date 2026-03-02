6dd15f0cc2ce8470ba753eae879c441e195c0986
Souma <git@5ouma.me>
2026-03-02 22:22:03 +0900

    fix(src): Use the icon URL from meta file
    Use the same image as other tests.

--- src/components/Bio/stories.ts
+++ src/components/Bio/stories.ts
@@ -3,18 +3,18 @@ import type { Props } from "./Bio.astro";
 export const Default: Props = {
   name: "Bio Name",
   description: "Bio Description",
-  icon: "https://avatars.githubusercontent.com/withastro",
+  icon: "https://avatars.githubusercontent.com/5ouma",
 };
 
 export const Japanese: Props = {
   name: "名前",
   description: "説明",
-  icon: "https://avatars.githubusercontent.com/withastro",
+  icon: "https://avatars.githubusercontent.com/5ouma",
 };
 
 export const Long: Props = {
   name: "Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Bio Name",
   description:
     "Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Bio Description",
-  icon: "https://avatars.githubusercontent.com/withastro",
+  icon: "https://avatars.githubusercontent.com/5ouma",
 };
