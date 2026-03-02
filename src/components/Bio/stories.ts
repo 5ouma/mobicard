fba27c85ddcf5af6975ae8ef062a1ae181981a01
Souma <git@5ouma.me>
2026-03-02 22:10:50 +0900

    fix(src): Use the icon URL that's redirected (#624)
    Astro now doesn't allow redirection to infer size.

--- src/components/Bio/stories.ts
+++ src/components/Bio/stories.ts
@@ -3,18 +3,18 @@ import type { Props } from "./Bio.astro";
 export const Default: Props = {
   name: "Bio Name",
   description: "Bio Description",
-  icon: "https://github.com/withastro.png",
+  icon: "https://avatars.githubusercontent.com/withastro",
 };
 
 export const Japanese: Props = {
   name: "名前",
   description: "説明",
-  icon: "https://github.com/withastro.png",
+  icon: "https://avatars.githubusercontent.com/withastro",
 };
 
 export const Long: Props = {
   name: "Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Bio Name",
   description:
     "Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Long Bio Description",
-  icon: "https://github.com/withastro.png",
+  icon: "https://avatars.githubusercontent.com/withastro",
 };
